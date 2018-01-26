efb6a946edf78523f8d55d4ee3cd922eaadd8616
Lee Ju Young <ju1115kr@naver.com>
2018-01-26 14:28:32 +0900

    add ajaxQuery function in js file

--- client/js/facebookLogin.js
+++ client/js/facebookLogin.js
@@ -1,29 +1,53 @@
+var API = 'http://ju1115kr.iptime.org:9009/api/v1.0';
+
+function ajaxQuery(type, apiURL, dataset) {
+    $.ajax({type: type,
+        url: API + apiURL,
+        contentType: 'application/json; charset=utf-8',
+        traditional: true,
+        data: JSON.stringify(dataset),
+        success: function(data){ console.log(data); return(data);},
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status, ajaxOptions, thrownError);
+            return(false);
+        }
+    });
+}
+
 function ajaxTest(){
     var SendInfo = { author_id:"123", context:"test", x:0, y:0 };
 
-    //    $.ajax({
-    //      type: 'post',
-    //      url: 'http://ju1115kr.iptime.org:9009/api/v1.0/particle',
-    //      data: JSON.stringify(SendInfo),
-    //      contentType: "application/json; charset=utf-8",
-    //      traditional: true,
-    //      success: function(data, response){
+    ajaxQuery(type='get', apiURL='/particle', dataset=SendInfo);
+
+    //$.ajax({
+    //    type: 'post',
+    //    url: 'http://ju1115kr.iptime.org:9009/api/v1.0/particle',
+    //    data: JSON.stringify(SendInfo),
+    //    contentType: "application/json; charset=utf-8",
+    //    traditional: true,
+    //    success: function(data, response){
     //        console.log(data);
     //        console.log(response);
-    //      }
-    //    });
+    //    }
+    //});
 
-    $.ajax({
-        type: 'get',
-        url: 'http://ju1115kr.iptime.org:9009/api/v1.0/particle',
-        contentType: 'application/json; charset=utf-8',
-        traditional: true,
-        success: function(data, response){
-            console.log(data.particles);
-            console.log(data.particles[0].created_at);
-            console.log(response);
-        }
-    });
+    //$.ajax({
+    //    type: 'get',
+    //    url: 'http://ju1115kr.iptime.org:9009/api/v1.0/particle',
+    //    contentType: 'application/json; charset=utf-8',
+    //    traditional: true,
+    //    success: function(data, response){
+    //        if (data !== "[]") {
+    //            console.log(data.particles);
+    //            for(i=0; i<data.particles.length; i++) {
+    //                var date = new Date(data.particles[i].created_at);
+    //                console.log(date.toLocaleDateString());
+    //                console.log(date.toLocaleTimeString());
+    //            }
+    //        }
+    //        else { console.log("No data inside"); }
+    //    }
+    //});
 }
 
 ajaxTest();
